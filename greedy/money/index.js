a3eaa782a3c27c96089d172548f24291e101ef83
lkm36486938 <ksan3940@gmail.com>
2021-10-24 18:48:25 +0900

    문제 의도에 맞게 로직 재수정

--- greedy/money/index.js
+++ greedy/money/index.js
@@ -1,51 +1,29 @@
 "use strict";
 
 let input = document.getElementById("input");
-let h_500 = document.getElementById("500");
-let h_100 = document.getElementById("100");
-let h_50 = document.getElementById("50");
-let h_10 = document.getElementById("10");
+let h1 = document.getElementById("h1");
 
 /** 그리디 알고리즘의 가장 적합한 문제인 동전 수 구하기 문제이다.
  * 알고리즘 공부가 목적이기 때문에 입력값은 항상 정확한 값을 집어넣는다고 가정하고 코드를 작성한다. (방어코드없음)
  */
 function solution() {
-    let value = Number(input.value);
-    let coinObj = {
-        500: 0,
-        100: 0,
-        50: 0,
-        10: 0,
-    };
+    let value = Number(input.value); // 거스름 돈
+    let result = 0; // 거슬러줘야하는 동전수의 총 합
 
-    // 1260 = 500*2 100*2 50*1 10*1
-    while (true) {
-        console.log(value);
-        if (value == 0) break;
-        if (value >= 500) {
-            ++coinObj[500];
-            value -= 500;
-            continue;
-        }
-        if (value >= 100) {
-            ++coinObj[100];
-            value -= 100;
-            continue;
-        }
-        if (value >= 50) {
-            ++coinObj[50];
-            value -= 50;
-            continue;
-        }
-        if (value >= 10) {
-            ++coinObj[10];
-            value -= 10;
-            continue;
-        }
+    let coinTypes = [500, 100, 50, 10]; // 동전배열
+
+    for (const coin of coinTypes) {
+        // 동전 수 구하기
+        result += Math.floor(value / coin);
+
+        // 코인 수 표기
+        document.getElementById(
+            "span_" + coin
+        ).textContent = `${coin}원: ${Math.floor(value / coin)}개`;
+
+        // value 값 수정
+        value %= coin;
     }
 
-    h_500.textContent = `500원: ${coinObj[500]}개`;
-    h_100.textContent = `100원: ${coinObj[100]}개`;
-    h_50.textContent = `50원: ${coinObj[50]}개`;
-    h_10.textContent = `10원: ${coinObj[10]}개`;
+    h1.textContent = result;
 }
