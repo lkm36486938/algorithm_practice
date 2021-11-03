699b1e2ab50072df5f55e9ae61bc26d52d67180f
lkm36486938 <ksan3940@gmail.com>
2021-11-03 22:04:12 +0900

    문자열 재정렬

---
+++ implemented/string_rearrangement/index.js
@@ -0,0 +1,31 @@
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.setPrompt(">");
+rl.prompt();
+rl.on("line", (S) => {
+    let result = solution(S);
+    console.log(`답: ${result}`);
+});
+
+rl.on("close", () => {
+    process.exit();
+});
+
+function solution(S) {
+    console.log(`S: ${S}`);
+
+    let number = 0;
+    let str = [];
+    for (const char of S) {
+        // 문자일 경우
+        if (isNaN(char)) {
+            str.push(char);
+        } else {
+            number += parseInt(char);
+        }
+    }
+    return str.sort((a, b) => (a > b ? 1 : -1)).join("") + number;
+}
