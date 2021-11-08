d74241d5824e85038dc394357699605a42bffe40
lkm36486938 <ksan3940@gmail.com>
2021-11-08 17:42:24 +0900

    간소화

--- sort/select/index.js
+++ sort/select/index.js
@@ -1,24 +1,14 @@
-function selectionSort(array) {
-    for (let i = 0; i < array.length; i++) {
-        let minIndex = i;
+let array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8];
 
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[minIndex] > array[j]) {
-                minIndex = j;
-            }
-        }
+for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[minIndex] > array[j]) minIndex = j;
 
-        if (minIndex !== i) {
-            let swap = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = swap;
-        }
-        console.log(`${i}회전: ${array}`);
+        let swap = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = swap;
     }
-
-    console.log(array);
-    return array;
 }
 
-let array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8];
-selectionSort(array);
+console.log(array);
