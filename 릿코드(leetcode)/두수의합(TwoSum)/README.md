0fe77bcc0da0cb521e19dbafd6527e43724eae34
leekyoungmin121 <kmlee@hdtel.co.kr>
2022-01-26 14:40:36 +0900

    leetcode-TwoSum

---
+++ 릿코드(leetcode)/두수의합(TwoSum)/README.md
@@ -0,0 +1,6 @@
+# TwoSum
+링크: <a href="https://leetcode.com/problems/two-sum/">leetcode - TwoSum</a>
+<hr/>
+<a href="./test.js">풀이.js</a>
+<hr/>
+<img src='./solution.PNG' />
---
+++ 릿코드(leetcode)/두수의합(TwoSum)/test.js
@@ -0,0 +1,17 @@
+var twoSum = function (nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let curNum = nums[i];
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j)
+        if (curNum + nums[j] === target) {
+          if (result.length === 0) result = [i, j];
+        }
+    }
+  }
+
+  return result;
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
