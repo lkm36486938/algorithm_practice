cf6543a9c4ce73b4c3e55ca267ebe111f6f6d246
lkm36486938 <ksan3940@gmail.com>
2022-02-14 16:27:55 +0900

    4. Median of Two Sorted Arrays

---
+++ 릿코드(leetcode)/4. Median of Two Sorted Arrays/test.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+  let arr = nums1.concat(nums2).sort((a, b) => a - b);
+  return arr.length % 2 == 1
+    ? arr[Math.floor(arr.length / 2)]
+    : (arr[Math.floor(arr.length / 2 - 1)] + arr[Math.floor(arr.length / 2)]) / 2;
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
+console.log(findMedianSortedArrays([3], [-2, -1]));
